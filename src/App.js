712faeb72b4d6bcac8a964a8858ea18deff5a1bc
Niruth Ananth <niruthananth03@gmail.com>
2024-09-05 15:35:08 +0530

    Testimonial page done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 // import PageNotFound from './components/PageNotFound';
 import Poster1 from "./LandingPage/components/Poster1";
 import Poster2 from "./LandingPage/components/Poster2";
+import Testimonial from "./LandingPage/components/Testimonial";
 
 function App() {
   return (
     <>
       <div className="flex flex-col gap-3">
+        <Testimonial/>
         <Poster1 />
         <Poster2 />
       </div>
