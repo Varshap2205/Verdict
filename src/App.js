e71ff70fb39152743bf9a91105e545165e152fd4
HansaSingh <hansasingh123434@gmail.com>
2024-09-11 18:05:37 +0530

    faq

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Auth from "./Auth/Auth";
 import Home from "./LandingPage/components/Home";
 import AiBot from "./Aibot/components/AiBot";
 
-import Faq from "./LandingPage/components/Faq";
+//import Faq from "./LandingPage/components/Faq";
 import PageNotFound from "./components/PageNotFound";
 
 function App() {
@@ -15,7 +15,6 @@ function App() {
         <Route path="/login" element={<Auth />} />
         <Route path="/aibot" element={<AiBot />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/faq" element={<Faq />} />
       </Routes>
     </>
   );
