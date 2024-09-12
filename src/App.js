6b76de218a14d250ee8e8c624f4a3956c4cd02a9
Niruth Ananth <niruthananth03@gmail.com>
2024-09-12 21:09:57 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Auth from './Auth/User';
 import Home from './LandingPage/components/Home';
-import { AiBot } from './Aibot/AiBot';
+import  AiBot  from "./Aibot/components/AiBot"
 import Faq from './LandingPage/components/Faq';
 import PageNotFound from './components/PageNotFound';
 import Sidebar1 from "./Dashboard/components/Sidebar1";
