f5cc99d208d266567558d3ed80c543bc7888d48b
HansaSingh <hansasingh123434@gmail.com>
2024-09-14 05:46:49 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Auth from "./Auth/Auth";
+import Auth from "./Auth/User";
 import Home from "./LandingPage/components/Home";
 
 import AiBot from "./Aibot/components/AiBot";
@@ -11,53 +10,30 @@ import PageNotFound from "./components/PageNotFound";
 
 import Faq from "./LandingPage/components/Faq";
 
-=======
-import React from 'react';
-import { Route,Routes } from 'react-router-dom';
-import Auth from './Auth/User';
-import Home from './LandingPage/components/Home';
-import { AiBot } from './Aibot/AiBot';
-import Faq from './LandingPage/components/Faq'
-import PageNotFound from './components/PageNotFound'
->>>>>>> 9e4ba2a3c2116c43c64012567b013f2549e7ced3
 import Sidebar1 from "./Dashboard/components/Sidebar1";
-<<<<<<< HEAD
-import Page from './Auth/Page';
-import Loyer from './Auth/Loyer';
 
-
-=======
+import Page from "./Auth/Page";
+import Loyer from "./Auth/Loyer";
+import EditProfile from "./Dashboard/components/EditProfile";
 import Profile from "./Dashboard/components/Profile";
->>>>>>> f3f47e74c2ea891568857a0cc058cfe5f5d3cb12
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path='/login' element={<Auth/>}/>
-        
-        <Route path="/auth" element={<Page />} >
-          <Route path='user' element={<Auth/>}/>
-          <Route path='loyer' element={<Loyer/>}/>
+        <Route path="/login" element={<Auth />} />
+        <Route path="/auth" element={<Page />}>
+          <Route path="user" element={<Auth />} />
+          <Route path="loyer" element={<Loyer />} />
         </Route>
 
-=======
-        <Route path="/login" element={<Auth />} />
-<<<<<<< HEAD
         <Route path="/aibot" element={<AiBot />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="faq" element={<Faq />} />
         <Route path="/profile" element={<Sidebar1 />} />
         <Route path="/profile1" element={<Profile />} />
-=======
->>>>>>> f3f47e74c2ea891568857a0cc058cfe5f5d3cb12
-        <Route path="/aibot" element={<AiBot/>}/>
-        <Route path="aibot" element={<AiBot />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path='faq' element={<Faq/>}/>
-        <Route path='/profile' element={<Sidebar1/>}/>
->>>>>>> 9e4ba2a3c2116c43c64012567b013f2549e7ced3
+        <Route path="/Edit" element={<EditProfile />} />
       </Routes>
     </>
   );
--- src/Dashboard/components/Profile.jsx
+++ src/Dashboard/components/Profile.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-//import image1 from "./Assets/image1.avif";
-
+import profile from "../Assets/image1.avif";
+import { Link } from "react-router-dom";
 function Profile() {
   return (
     <>
-      <div className="border w-full h-full mt-5  ">
-        <div className="flex flex-row space-between ">
-          <div className="bg-gray-900  border rounded-full w-64 h-64  mx-20 my-20">
-            <img className=" " alt=""></img>
+      <div className="border-b border-gray-500 w-full h-full">
+        <div className="flex flex-col lg:gap-16 md:flex-row md:space-between">
+          <div className="bg-gray-900 w-48 h-48 md:w-64 md:h-64 rounded-full overflow-hidden flex justify-center items-center mx-auto md:ml-20 md:mr-40 my-10 md:my-20">
+            <img
+              className="w-full h-full object-cover"
+              src={profile}
+              alt="Profile"
+            ></img>
           </div>
 
-          <div></div>
+          <div className="flex flex-col text-center md:text-left">
+            <h1 className="lg:mt-20 lg:mb-20  mb-5 text-4xl md:text-5xl mx-auto md:mx-40">
+              David
+            </h1>
+            <div className="flex flex-col md:flex-row justify-center items-center gap-4 md:gap-12  mb-8">
+              <Link to="/Edit" className="">
+                {" "}
+                <button className="px-16 lg:px-20 flex text-lg justify-center py-3 bg-gray-500 w-1/3 text-white font-bold rounded-md ml-10">
+                  Edit
+                </button>
+              </Link>
+              <button className="px-5 py-3 bg-gray-500 w-1/3 text-white font-bold rounded-md text-lg">
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
