2c15a1f895bc2722bae7c93614e23b23fd9a6757
HansaSingh <hansasingh123434@gmail.com>
2024-09-15 10:47:52 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Auth from "./Auth/User";
@@ -6,7 +5,6 @@ import Home from "./LandingPage/components/Home";
 
 import AiBot from "./Aibot/components/AiBot";
 
-//import Faq from "./LandingPage/components/Faq";
 import PageNotFound from "./components/PageNotFound";
 
 import Faq from "./LandingPage/components/Faq";
@@ -18,52 +16,32 @@ import Loyer from "./Auth/Loyer";
 import EditProfile from "./Dashboard/components/EditProfile";
 import Profile from "./Dashboard/components/Profile";
 
-=======
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Auth from './Auth/User';
-import Home from './LandingPage/components/Home';
-import  AiBot  from "./Aibot/components/AiBot"
-import Faq from './LandingPage/components/Faq';
-import PageNotFound from './components/PageNotFound';
-import Sidebar1 from "./Dashboard/components/Sidebar1";
-import Profile from "./Dashboard/components/Profile";
-import Page from './Auth/Page';
-import Loyer from './Auth/Loyer';
-
->>>>>>> 6b76de218a14d250ee8e8c624f4a3956c4cd02a9
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Auth />} />
-<<<<<<< HEAD
+
         <Route path="/auth" element={<Page />}>
           <Route path="user" element={<Auth />} />
           <Route path="loyer" element={<Loyer />} />
         </Route>
 
-=======
-        
         {/* Nested route structure under /auth */}
-        <Route path="/auth" element={<Page />} >
+        <Route path="/auth" element={<Page />}>
           <Route path="user" element={<Auth />} />
           <Route path="loyer" element={<Loyer />} />
         </Route>
-        
->>>>>>> 6b76de218a14d250ee8e8c624f4a3956c4cd02a9
+
         <Route path="/aibot" element={<AiBot />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/profile" element={<Sidebar1 />} />
         <Route path="/profile1" element={<Profile />} />
-<<<<<<< HEAD
-        <Route path="/Edit" element={<EditProfile />} />
-=======
-        
-        {/* Catch-all route for 404 errors */}
         <Route path="*" element={<PageNotFound />} />
->>>>>>> 6b76de218a14d250ee8e8c624f4a3956c4cd02a9
+        <Route path="/Edit" element={<EditProfile />} />
+
+        {/* Catch-all route for 404 errors */}
       </Routes>
     </>
   );
