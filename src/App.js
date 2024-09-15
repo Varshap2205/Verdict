102222e5d9c7d86eb9acebd28a046c5803d3fba3
Niruth Ananth <niruthananth03@gmail.com>
2024-09-15 13:34:46 +0530

    Features part done

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Loyer from "./Auth/Loyer";
 import EditProfile from "./Dashboard/components/EditProfile";
 import ProfilePage from "./Dashboard/ProfilePage";
 import HTU from '../src/HTU/HowToUSe'
+import Blog from "../src/Blog/Blog"
 
 function App() {
   return (
@@ -18,20 +19,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Auth />} />
-
         {/* Nested route structure under /auth */}
         <Route path="/auth" element={<Page />}>
           <Route path="user" element={<Auth />} />
           <Route path="loyer" element={<Loyer />} />
         </Route>
-
         <Route path="/aibot" element={<AiBot />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile1" element={<Profile />} />
         <Route path="/edit" element={<EditProfile />} />
         <Route path='howitworks' element={<HTU/>}/>
-
+        <Route path='/blog' element={<Blog/>} />
         {/* Catch-all route for 404 errors */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/LandingPage/components/Features.jsx
+++ src/LandingPage/components/Features.jsx
@@ -1,78 +1,42 @@
 import React from 'react'
-import pics1 from './assets/pics1.jpeg'
-import pics2 from './assets/pics2.jpeg'
-import pics3 from './assets/pics3.jpeg'
-import pics4 from './assets/pics4.jpeg'
+import { GoLaw } from "react-icons/go";
+import { SiWritedotas } from "react-icons/si";
+import { FaPeopleLine } from "react-icons/fa6";
 
 function Features() {
   return (
-    <div className='bg-black'>
-      <div className='flex justify-center items-center pt-5 text-3xl sm:text-5xl md:text-6xl font-bold font-mono text-white'>
-        Features
-      </div>
-
-      <div className='flex flex-wrap justify-center items-center text-white overflow-hidden'>
-      
-        <div className='bg-blue-500 w-64 sm:w-72 h-auto m-5 rounded-md relative group'>
-          <img
-            src={pics1}
-            alt="User Rights Awareness"
-            className='rounded-full w-24 h-24 sm:w-32 sm:h-32 block mx-auto p-1 transition-transform duration-300 ease-in-out transform group-hover:scale-110 group-hover:shadow-lg'
-          />
-          <div className="absolute inset-0 border-2 border-transparent group-hover:border-white group-hover:rounded-lg transition-all duration-300 ease-in-out"></div>
-          <h1 className='font-mono font-bold text-xl sm:text-2xl text-center mt-4'>User Rights Awareness</h1>
-          <p className='text-sm sm:text-lg p-3 text-center'>
-            Users can access detailed information about their rights in consumer court cases and commercial disputes.
-          </p>
-        </div>
-
-        <div className='bg-blue-500 w-64 sm:w-72 h-auto m-5 rounded-md relative group'>
-          <img
-            src={pics2}
-            alt="Interactive Chatbot"
-            className='rounded-full w-24 h-24 sm:w-32 sm:h-32 block mx-auto p-1 transition-transform duration-300 ease-in-out transform group-hover:scale-110 group-hover:shadow-lg'
-          />
-          <div className="absolute inset-0 border-2 border-transparent group-hover:border-white group-hover:rounded-lg transition-all duration-300 ease-in-out"></div>
-          <h1 className='font-mono font-bold text-xl sm:text-2xl text-center mt-4'>Interactive Chatbot</h1>
-          <p className='text-sm sm:text-lg p-3 text-center'>
-            An AI-powered chatbot assists users by answering frequently asked legal questions related to consumer courts and helps guide them through their legal queries.
-          </p>
-        </div>
-
-        <div className='bg-blue-500 w-64 sm:w-72 h-auto m-5 rounded-md relative group'>
-          <img
-            src={pics3}
-            alt="Case Management"
-            className='rounded-full w-24 h-24 sm:w-32 sm:h-32 block mx-auto p-1 transition-transform duration-300 ease-in-out transform group-hover:scale-110 group-hover:shadow-lg'
-          />
-          <div className="absolute inset-0 border-2 border-transparent group-hover:border-white group-hover:rounded-lg transition-all duration-300 ease-in-out"></div>
-          <h1 className='font-mono font-bold text-xl sm:text-2xl text-center mt-4'>Case Management</h1>
-          <div className='text-sm sm:text-lg p-3 text-center'>
-            Users' case statuses are visually represented:
-            <ul className='list-disc list-inside'>
-              <li>Red: Case not yet solved or assigned.</li>
-              <li>Orange: Case is taken by a law practitioner and is being worked on.</li>
-              <li>Green: Case successfully resolved.</li>
-            </ul>
+    <section className='w-[95%] mx-auto'>
+      <h1 className="text-center mb-5 capitalize pl-7 font-bold text-4xl md:text-5xl">Features</h1>
+      <div className='grid grid-cols md:grid-cols-3 gap-3'>
+        {FeatureData.map((e) => (
+          <div key={e.id} className='bg-[#3c4043] text-white flex flex-col justify-center items-center py-2 px-3 rounded-lg'>
+            <button className='flex items-center'>{e.photo}</button>
+            <p className='bg-[#3c4043] text-center text-xl'>{e.name}</p>
+            <p className='bg-[#3c4043] mt-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe ducimus distinctio nisi fugit officia blanditiis laborum sunt, velit eaque cupiditate necessitatibus harum, eligendi consequuntur atque repellendus veniam. Delectus, nulla deserunt!</p>
           </div>
-        </div>
-
-        <div className='bg-blue-500 w-64 sm:w-72 h-auto m-5 rounded-md relative group'>
-          <img
-            src={pics4}
-            alt="Connection with Law Practitioners"
-            className='rounded-full w-24 h-24 sm:w-32 sm:h-32 block mx-auto p-1 transition-transform duration-300 ease-in-out transform group-hover:scale-110 group-hover:shadow-lg'
-          />
-          <div className="absolute inset-0 border-2 border-transparent group-hover:border-white group-hover:rounded-lg transition-all duration-300 ease-in-out"></div>
-          <h1 className='font-mono font-bold text-xl sm:text-2xl text-center mt-4'>Connection with Law Practitioners</h1>
-          <p className='text-sm sm:text-lg p-3 text-center'>
-            The platform connects users with verified law practitioners who can take up their cases and help resolve them.
-          </p>
-        </div>
-
+        ))}
       </div>
-    </div>
-  );
+    </section>
+  )
+
 }
 
-export default Features;
+export default Features
+
+const FeatureData = [
+  {
+    id: 1,
+    photo: <GoLaw size={60} className='bg-[#3c4043]' />,
+    name: "AI based Laywer"
+  },
+  {
+    id: 2,
+    photo: <SiWritedotas size={60} className='bg-[#3c4043]' />,
+    name: "Blogs"
+  },
+  {
+    id: 1,
+    photo: <FaPeopleLine size={60} className='bg-[#3c4043]' />,
+    name: "Hire lawyers"
+  }
+]
